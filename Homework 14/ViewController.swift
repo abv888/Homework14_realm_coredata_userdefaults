668247146d15cda05aaa352a93c491eda445009e
Bagrat Arutyunov <Arutyunovbagrat@gmail.com>
2021-01-28 13:07:27 +0300

    Initial Commit

---
+++ Homework 14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 14
+//
+//  Created by  Bagrat Arutyunov on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
